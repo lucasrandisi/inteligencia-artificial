d91ccae0c4a24c287a099b209e5e993b3ac9f71d
Lucas Randisi <lucasrandisi@gmail.com>
2022-07-02 23:29:46 -0300

    Breadth first search

--- tp-2/vuelos.pl
+++ tp-2/vuelos.pl
@@ -1,23 +1,26 @@
-vuelo( salta , cordoba ,80).
-vuelo( cordoba , formosa ,120).
-vuelo( cordoba , rosario ,55).
-vuelo( cordoba , neuquen ,100).
-vuelo( cordoba , mendoza ,65).
-vuelo( formosa , corrientes ,50).
-vuelo( corrientes , posadas ,70).
-vuelo( corrientes ,  parana ,57).
-vuelo( corrientes , santaFe ,93).
-vuelo( posadas , parana ,85).
-vuelo( santaFe , rosario ,30).
-vuelo( rosario , parana ,25).
-vuelo( rosario , buenosAires ,55).
-vuelo( rosario , mendoza ,175).
-vuelo( mendoza , buenosAires ,85).
-vuelo( mendoza , neuquen ,83).
-vuelo( neuquen ,  buenosAires ,90).
-vuelo( buenosAires , calafate ,115).
-vuelo( buenosAires ,  puertoMadryn ,75).
-vuelo( neuquen , calafate ,65).
+vuelo(salta, cordoba, 80).
+vuelo(cordoba, formosa, 120).
+vuelo(cordoba, rosario, 55).
+vuelo(cordoba, neuquen, 100).
+vuelo(cordoba, mendoza, 65).
+vuelo(formosa, corrientes, 50).
+vuelo(corrientes, posadas, 70).
+vuelo(corrientes,  parana, 57).
+vuelo(corrientes, santaFe, 93).
+vuelo(posadas, parana, 85).
+vuelo(santaFe, rosario, 30).
+vuelo(rosario, parana, 25).
+vuelo(rosario, buenosAires, 55).
+vuelo(rosario, mendoza, 175).
+vuelo(mendoza, buenosAires, 85).
+vuelo(mendoza, neuquen, 83).
+vuelo(neuquen,  buenosAires, 90).
+vuelo(buenosAires, calafate, 115).
+vuelo(buenosAires,  puertoMadryn, 75).
+vuelo(neuquen, calafate, 65).
+
+conectadas(A, B) :- vuelo(A, B, _).
+conectadas(A, B) :- vuelo(B, A, _).
 
 
 init :- 
@@ -29,7 +32,7 @@ init :-
 
 rutas(Origen, Destino, MinTrasbordos) :- 
 	MinTrasbordos = s, 
-	breadhSearch(Origen, Destino, Ruta, Tiempo),
+	breadthFirstSearch([[Origen]], Destino, Ruta),
 	write('Ruta:\n'), write(Ruta).
 rutas(Origen, Destino, MinTrasbordos) :- 
 	MinTrasbordos = n, 
@@ -38,15 +41,34 @@ rutas(Origen, Destino, MinTrasbordos) :-
 
 
 
-breadhSearch(Origen, Origen, [Origen], 0).
+breadthFirstSearch([[Destino|Ruta]|_], Destino, [Destino|Ruta]).
+breadthFirstSearch([PrimerRuta|OtrasRutas], Destino, Ruta) :-
+	hijosNodo(PrimerRuta, NuevasRutas),
+	concatenar(OtrasRutas, NuevasRutas, NuevaListaNodos),
+	breadthFirstSearch(NuevaListaNodos, Destino, Ruta).
+
+
+hijosNodo([Nodo|Ruta], Hijos) :-
+	findall(
+		[Hijo,Nodo|Ruta], 
+		(
+			conectadas(Nodo, Hijo),
+			not(in(Hijo, [Nodo|Ruta]))
+		),
+		Hijos
+	).
+hijosNodo(_, []).
+
+
+concatenar([], B, B).
+concatenar([H|T], B, [H|C]) :- concatenar(T, B, C).
 
 
 deepFirstSearch(Origen, Origen, Ruta, Ruta).
 deepFirstSearch(Origen, Destino, RutaParcial, Ruta) :- 
-	vuelo(Origen, Ciudad, _),
+	conectadas(Origen, Ciudad),
 	not(in(Ciudad, RutaParcial)),
 	deepFirstSearch(Ciudad, Destino, [Ciudad|RutaParcial], Ruta).
-
 
 
 in(X, [X|_]).
