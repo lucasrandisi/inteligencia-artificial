9191c2ce031ab1b35c02acb39c90df334cbe3d54
Lucas Randisi <lucasrandisi@gmail.com>
2022-09-06 17:24:40 -0300

    tp4 reordenar nodos de decisión

--- tp-4/turismo.pl
+++ tp-4/turismo.pl
@@ -1,11 +1,11 @@
 :- dynamic(menores/2).
-:- dynamic(costoSemanaPersona/2).
+:- dynamic(costoDiaPersona/2).
 :- dynamic(mesRecomendado/2).
 :- dynamic(tipoDestino/2).
 :- dynamic(clima/2).
-:- dynamic(vuelo/3).
+:- dynamic(vuelo/4).
 
-inicio:- 
+inicio:-
 	cargarDatos,
 	leerOpciones(Presupuesto, CantidadPersonas, CantidadDias, Menores, Mes, TipoDestino, Clima, TiempoViajeMaximo, CiudadOrigen),
 	recomendarViaje(Presupuesto, CantidadPersonas, CantidadDias, Menores, Mes, TipoDestino, Clima, TiempoViajeMaximo, CiudadOrigen).
@@ -14,7 +14,7 @@ inicio:-
 cargarDatos :- consult('/home/lucas/Facultad/Inteligencia Artificial/tp-4/ciudades.txt').
 
 
-leerOpciones(Presupuesto, CantidadPersonas, CantidadDias, Menores, Mes, TipoDestino, Clima, TiempoViajeMaximo, CiudadOrigen) :- 
+leerOpciones(Presupuesto, CantidadPersonas, CantidadDias, Menores, Mes, TipoDestino, Clima, TiempoViajeMaximo, CiudadOrigen) :-
 	writeln('Presupuesto:'),
 	read(Presupuesto),
 	writeln('Cantidad de Personas:'),
@@ -26,33 +26,37 @@ leerOpciones(Presupuesto, CantidadPersonas, CantidadDias, Menores, Mes, TipoDest
 	writeln('Mes en el que tiene pensado viajar: '),
 	read(Mes),
 	writeln('Tipo de Destino (ciudad o exteriores):'),
-	read(TipoDestino), 
+	read(TipoDestino),
 	writeln('Clima:'),
 	read(Clima),
-	writeln('Máximo tiempo de viaje:'),
+	writeln('Máximo tiempo de viaje en hs:'),
 	read(TiempoViajeMaximo),
-	writeln('Ciudad de Origen:'),
+	writeln('Ciudad de Origen:' ),
 	read(CiudadOrigen).
 
 
 recomendarViaje(Presupuesto, CantidadPersonas, CantidadDias, Menores, Mes, TipoDestino, Clima, TiempoViajeMaximo, CiudadOrigen) :-
-	menores(Destino, Menores),
-	costoSemanaPersona(Destino, CostoDiaPersona),
-	CostoTotal is CostoDiaPersona * CantidadPersonas * CantidadDias,
-	Presupuesto >= CostoTotal,
-	mesRecomendado(Destino, Mes),
 	tipoDestino(Destino, TipoDestino),
 	clima(Destino, Clima),
-	breadthFirstSearch(CiudadOrigen, Destino, RutaFinal),
+	menores(Destino, Menores),
+	mesRecomendado(Destino, Mes),
+
+	costoDiaPersona(Destino, CostoDiaPersona),
+	CostoTotal is CostoDiaPersona * CantidadPersonas * CantidadDias,
+	Presupuesto >= CostoTotal,
+
+	breadthFirstSearch([[CiudadOrigen]], Destino, RutaFinal),
 	tiempoViaje(RutaFinal, TiempoViaje),
 	TiempoViajeMaximo >= TiempoViaje,
+
+
 	writeln('Destino Recomendado: '),
 	write(Destino).
 recomendarViaje(_, _, _, _, _, _, _, _):- writeln('No se ha encontrado un destino para sus preferencias').
 
 
 
-breadthFirstSearch([[Destino|Ruta]|_], Destino, RutaFinal) :- 
+breadthFirstSearch([[Destino|Ruta]|_], Destino, RutaFinal) :-
 	reverse([Destino|Ruta], RutaFinal).
 breadthFirstSearch([PrimerRuta|OtrasRutas], Destino, Ruta) :-
 	hijosNodo(PrimerRuta, NuevasRutas),
@@ -60,14 +64,14 @@ breadthFirstSearch([PrimerRuta|OtrasRutas], Destino, Ruta) :-
 	breadthFirstSearch(NuevaListaNodos, Destino, Ruta).
 
 % Verificar si 2 ciudades se encuentran conectadas
-conectadas(A, B, Tiempo) :- vuelo(A, B, Tiempo).
-conectadas(A, B, Tiempo) :- vuelo(B, A, Tiempo).
+conectadas(A, B, Tiempo) :- vuelo(A, B, Tiempo, _).
+conectadas(A, B, Tiempo) :- vuelo(B, A, Tiempo, _).
 
 
 % Ciudades conectadas que se no se encuentren en la ruta actual
 hijosNodo([Nodo|Ruta], Hijos) :-
 	findall(
-		[Hijo,Nodo|Ruta], 
+		[Hijo,Nodo|Ruta],
 		(
 			conectadas(Nodo, Hijo, _),
 			not(in(Hijo, [Nodo|Ruta]))
